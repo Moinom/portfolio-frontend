1263821abb225f48d5871dc0ae7a1a63dd506ec0
Lisa <st.lisa93@yahoo.de>
2022-12-17 00:03:03 +0100

    remove obsolete theme colour change on filter icon

--- src/components/artGallery/ArtGallery.tsx
+++ src/components/artGallery/ArtGallery.tsx
@@ -1,12 +1,10 @@
-import React, { useContext, useState } from 'react';
-import { ThemeContext, themes } from '../../contexts/ThemeProvider';
+import React, { useState } from 'react';
 import FilterPopUp from '../filterPopUp/FilterPopUp';
 import { artFilters } from '../filterPopUp/filters';
 import { ReactComponent as FilterIcon } from '../../assets/icons/filter.svg';
 import styles from './ArtGallery.module.css';
 
 const ArtGallery = () => {
-  const { theme } = useContext(ThemeContext);
   const [filters, setFilters] = useState(artFilters);
   const [isPopupVisible, setPopupVisible] = useState(false);
 
@@ -23,11 +21,7 @@ const ArtGallery = () => {
   return (
     <section>
       <div className={styles.filter} onClick={togglePopupVisibility}>
-        <FilterIcon
-          className={`${styles.filterIcon} ${
-            theme === themes.code ? styles.codeStrokeColour : ''
-          }`}
-        />
+        <FilterIcon className={styles.filterIcon} />
         <h1>Filter</h1>
       </div>
       {isPopupVisible && (
