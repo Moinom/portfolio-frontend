9a0a4b96327502f2aecb067435c6fdfe029fe55f
Lisa <st.lisa93@yahoo.de>
2023-04-02 22:57:43 +0200

    Moved backend to separate repository

--- backend/routes/artRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as artControllers from '../controllers/artControllers';
-const router = express.Router();
-
-router.get('/', artControllers.getArt);
-
-export default router;
--- backend/routes/codeRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as codeControllers from '../controllers/codeControllers';
-const router = express.Router();
-
-router.get('/', codeControllers.getCode);
-
-export default router;
