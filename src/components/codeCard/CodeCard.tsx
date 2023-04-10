aa0dee261a1c37733a38897a0d08640b95d2d848
Lisa <st.lisa93@yahoo.de>
2023-04-10 20:43:43 +0200

    add CodeCard test

--- src/components/codeCard/CodeCard.tsx
+++ src/components/codeCard/CodeCard.tsx
@@ -18,7 +18,7 @@ const CodeCard = (props: Code) => {
       <h2>{props.title}</h2>
       <img src={smallImageUrl()} alt={props.title} className={styles.image} />
       <p>{props.description}</p>
-      <p>
+      <p data-testid="techStack">
         <strong>Tech stack:</strong>{' '}
         {props.language
           ?.split(',')
@@ -28,11 +28,11 @@ const CodeCard = (props: Code) => {
       <div className={styles.links}>
         {props.prod && (
           <a href={props.prod} target="_blank" rel="noreferrer">
-            <ArrowRight /> <span>Check it out</span>
+            <ArrowRight title="arrow right" /> <span>Check it out</span>
           </a>
         )}
         <a href={props.github} target="_blank" rel="noreferrer">
-          <ArrowRight /> <span>View on Github</span>
+          <ArrowRight title="arrow right" /> <span>View on Github</span>
         </a>
       </div>
     </div>
